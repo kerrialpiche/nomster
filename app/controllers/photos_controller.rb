e9736208904c2977a6d3b9011f3bf4f34d0547f9
Kerri Alpiche <kerrialpiche@live.com>
2018-01-30 16:41:32 +0000

    trying to fix photos path

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,2 +1,13 @@
 class PhotosController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    @photo = Photo.find(params[:place_id])
+  end
+
+  private
+
+  def photo_params
+    params.require(:photo).permit(:caption)
+  end
 end
